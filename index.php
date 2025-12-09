6a5e5f2875c4c4663edd66d282a2cdc728bdaeb5
alicedetter <detteralice@gmail.com>
2025-12-09 14:48:49 +0100

    h

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="POST">
+        <input type="text" value="<?php ?>" />
+        <input type="submit" name="post.btn" value="Skicka" />
+    </form>
+</body>
+</html>
